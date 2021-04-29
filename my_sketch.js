9a58992b725b49aa6fb18dd45024b804af182a97
Lucy Laidlaw <lucyisabellelaidlaw@gmail.com>
2021-04-29 14:52:19 +1000

    0.2

--- my_sketch.js
+++ my_sketch.js
@@ -6,12 +6,5 @@ function setup() {
   // Top-left corner of the img is at (0, 0)
   // Width and height are the img's original width and height
   createCanvas(windowWidth, windowHeight);
-  image(img, 0, 0);
-}
-
-function draw() {
-
-  fill(color(random(255), random(255), random(255)));
-  ellipse(width/2, height/2, 300);
-
+  image(img, 0, 0, 960, 274);
 }
