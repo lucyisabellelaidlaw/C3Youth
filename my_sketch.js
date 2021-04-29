3907a2f24bf55cc9c0965b3d4a41cd8a18165258
Lucy Laidlaw <lucyisabellelaidlaw@gmail.com>
2021-04-29 21:05:17 +1000

    1.3 fixed

--- my_sketch.js
+++ my_sketch.js
@@ -129,7 +129,6 @@ function snowFlakesFall(snowFlakeXPosArray, snowFlakeYPosArray, snowFlakeSpeedAr
   fill(255);
   for(let i = 0; i < 20; i++) {
     size = snowFlakeSizeArray[i];
-    rotate(frameCount%360);
     image(img, snowFlakeXPosArray[i], (snowFlakeYPosArray[i]+frameCount*snowFlakeSpeedArray[i])%(height+100), size, size);
   }
 
