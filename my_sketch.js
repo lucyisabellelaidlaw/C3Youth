d4b3e35a07a5f73c083cf7f7b27488f67c599c83
Lucy Laidlaw <lucyisabellelaidlaw@gmail.com>
2021-04-29 14:57:42 +1000

    0.4

--- my_sketch.js
+++ my_sketch.js
@@ -6,13 +6,12 @@ function setup() {
   // Top-left corner of the img is at (0, 0)
   // Width and height are the img's original width and height
   createCanvas(windowWidth, windowHeight);
-  image(img, 0, height/2-(549)/2);
   background(color(68,91,214));
   noLoop();
 }
 
 function draw() {
 
-
+  image(img, 0, height/2-(549)/2);
 
 }
